8089f5020ce75c6722a0949baf5b9797fdfe3cc8
Ieshia Parker <ieshiak@icloud.com>
2024-02-02 23:35:00 -0500

    updated CommentTest

--- JPAArtGallery/src/test/java/com/skilldistillery/artgallery/entities/CommentTest.java
+++ JPAArtGallery/src/test/java/com/skilldistillery/artgallery/entities/CommentTest.java
@@ -2,6 +2,7 @@ package com.skilldistillery.artgallery.entities;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -16,11 +17,10 @@ import jakarta.persistence.Persistence;
 class CommentTest {
 
 	private static EntityManagerFactory emf;
-    private EntityManager em;
-    private Comment comment;
+	private EntityManager em;
+	private Comment comment;
 
-    
-    @BeforeAll
+	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 		emf = Persistence.createEntityManagerFactory("JPAArtGallery");
 	}
@@ -29,26 +29,31 @@ class CommentTest {
 	static void tearDownAfterClass() throws Exception {
 		emf.close();
 	}
-	
-	
-    @BeforeEach
-    void setUp() throws Exception {
-        em = emf.createEntityManager();
-        comment = em.find(Comment.class, 1);
-    }
 
-    @AfterEach
-    void tearDown() throws Exception {
-        em.close();
-        comment = null;
-    }
-    
-    @Test
-    void test_Comment_has_mappings() {
-    	assertNotNull(comment);
-    	assertNotNull(comment.getCommentText());
-    	assertEquals("whats up my boy", comment.getCommentText());
-    	
-    }
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		comment = em.find(Comment.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		comment = null;
+	}
+
+	@Test
+	void test_Comment_has_mappings() {
+		assertNotNull(comment);
+		assertNotNull(comment.getCommentText());
+		assertEquals("whats up my boy", comment.getCommentText());
+
+	}
+
+	@Test
+	void test_Comment_to_Artwork() {
+		assertNotNull(comment.getArtwork());
+		assertEquals(1, comment.getArtwork().getId());
+	}
 
 }
