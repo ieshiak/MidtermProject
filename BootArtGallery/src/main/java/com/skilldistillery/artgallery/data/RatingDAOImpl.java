9be72074b6c049da21440bca826742556b9bcd04
Ieshia Parker <ieshiak@icloud.com>
2024-02-05 17:30:50 -0500

    RatingDAO updated

--- BootArtGallery/src/main/java/com/skilldistillery/artgallery/data/RatingDAOImpl.java
+++ BootArtGallery/src/main/java/com/skilldistillery/artgallery/data/RatingDAOImpl.java
@@ -8,42 +8,56 @@ import com.skilldistillery.artgallery.entities.Rating;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
 import jakarta.transaction.Transactional;
 
 @Service
 @Transactional
-public class RatingDAOImpl implements RatingDAO{
+public class RatingDAOImpl implements RatingDAO {
 
 	@PersistenceContext
 	private EntityManager em;
 
 	@Override
 	public Rating findById(int ratingId) {
-		// TODO Auto-generated method stub
-		return null;
+		Rating managed = em.find(Rating.class, ratingId);
+		return managed;
 	}
 
 	@Override
 	public List<Rating> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		String jpql = "SELECT r FROM Rating r";
+		Query query = em.createQuery(jpql, Rating.class);
+		return query.getResultList();
 	}
 
 	@Override
 	public Rating create(Rating rating) {
-		// TODO Auto-generated method stub
-		return null;
+		em.persist(rating);
+		return rating;
 	}
 
 	@Override
 	public Rating update(Rating rating) {
-		// TODO Auto-generated method stub
-		return null;
+		Rating managed = em.find(Rating.class, rating.getId());
+		if (managed != null) {
+			managed.setRate(rating.getRate());
+			managed.setCreateTime(rating.getCreateTime());
+		}
+		return managed;
 	}
 
 	@Override
 	public boolean delete(int ratingId) {
-		// TODO Auto-generated method stub
+		Rating ratingToDelete = findById(ratingId);
+		if (ratingToDelete != null) {
+			try {
+				em.remove(ratingToDelete);
+				return true;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 		return false;
 	}
 
